6bba7c1922db05084032958421fa094ae57e0a72
Om Bodke <ombodke384@gmail.com>
2026-04-04 14:13:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: "https://skillbridgefrontend-taupe.vercel.app",
+  origin: [process.env.FRONTEND_URL || 'http://localhost:5173', 'https://skillbridgeprototype.vercel.app'],
   credentials: true
 }));
 app.use(express.json());
