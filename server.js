777ba3d4a19780fa4fdbf3a7bb6f1afe486fc145
Om Bodke <ombodke384@gmail.com>
2026-04-05 21:57:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,9 @@ app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
+//json
+app.use(express.json());
+
 // Routes
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/users', require('./routes/users'));
